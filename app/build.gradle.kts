330844ddeda20dd5972457b5f4d94f2cfdb5b46e
AlonsoAto <alonso.atoneyra@gmail.com>
2023-11-12 22:00:51 -0500

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get().toString()
     }
     packaging { resources.excludes.add("META-INF/*") }
 }
