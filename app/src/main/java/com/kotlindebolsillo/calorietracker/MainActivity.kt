936da600a13be78737805c7fed21a0d1c49889f5
AlonsoAto <alonso.atoneyra@gmail.com>
2023-11-13 00:00:40 -0500

    Remove unused resources

--- app/src/main/java/com/kotlindebolsillo/calorietracker/MainActivity.kt
+++ app/src/main/java/com/kotlindebolsillo/calorietracker/MainActivity.kt
@@ -3,14 +3,6 @@ package com.kotlindebolsillo.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.kotlindebolsillo.calorietracker.ui.theme.CalorieTrackerTheme
 import com.kotlindebolsillo.onboarding_presentation.welcome.WelcomeScreen
 
